8e58ad41cc42883db856cad4f273e9062c856e5b
Mayank Sharma <36720230+masharma2004@users.noreply.github.com>
2024-11-16 13:53:52 +0530

    solved api issue

--- src/Frontend/client/src/context/AuthContext.js
+++ src/Frontend/client/src/context/AuthContext.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 
 export const AuthContext = createContext();
 
-const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:5000';
+const API_URL = process.env.REACT_APP_API_URL;
 axios.defaults.baseURL = API_URL;
 axios.defaults.withCredentials = true;
 
